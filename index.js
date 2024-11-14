b09f4a8b6e53f805763bf4a432523ec052e9cfbe
g-ames <132623120+g-ames@users.noreply.github.com>
2024-11-13 22:34:23 -0500

    a few changes

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function constructCarousel() {
+var sfxTime = +Date.now();
 
+function animeGirl() {
+    if((+Date.now() - sfxTime) > 1000) {
+        sfxTime = +Date.now();
+        var sfx = ["cute-uwu.mp3", "anime-wow-sound-effect.mp3", "yes-daddy_CKEAffI.mp3"];
+        var effect = sfx[Math.round(Math.random()*(sfx.length-1))];
+        (new Audio(`/sfx/${effect}`)).play();
+    }
 }
+
+var backgroundDegrees = 0;
+var start = +Date.now();
+var last  = start;
+
+function update() {
+    last = start;
+    start = +Date.now();
+    backgroundDegrees -= ((start - last) / (80 + (Math.random() * 20)));
+    backgroundDegrees %= 360;
+    document.body.style.background = `linear-gradient(${backgroundDegrees}deg, rgba(4,1,18,1) 0%, rgb(17, 5, 44) 100%)`;
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
