79539c57530a6c993332fddd7d94ee88fc9fbb55
g-ames <132623120+g-ames@users.noreply.github.com>
2024-11-15 21:12:00 -0500

    fixes

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const http = require("http");
+const express = require("express");
+const path = require('path');
+const fs = require('node:fs');
+const app = express();
 
 const hostname = "127.0.0.1";
 const port = 8000;
 
+var constructedGamesListJSON = null;
+
+function constructGamesListJSON() {
+    const directoryPath = path.join(__dirname, 'Public/games');
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } else {
+            constructedGamesListJSON = files;
+        }
+    });
+}
+constructGamesListJSON();
+
+setInterval(() => constructGamesListJSON, 100000);
+
+app.get('/', (req, res) => {
+    if(constructedGamesListJSON != null) {
+        res.send(JSON.stringify([]));
+    } else {
+        res.send(JSON.stringify(constructedGamesListJSON));
+    }
+});
+
 app.use(express.static("public"));
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(req.path);
-});
-
-server.listen(port, hostname, function () {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
