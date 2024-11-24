96336146c3907f256b13cd5dce64c47ad11e4fc6
G-ames <olijones589@gmail.com>
2024-11-23 21:56:11 -0500

    epic text effect

--- public/featureUI.js
+++ public/featureUI.js
@@ -1,13 +1,13 @@
 var newDesc = document.getElementById("newDescriptor");
 var oldDesc = document.getElementById("oldDescriptor");
 var descriptors = [
-    "The Unmatched Game Library", 
-    "Speed and Performance", 
-    "Unstoppable Access", 
-    "The Responsive Design",
-    "Always Evolving",
-    "The Innovative Experience",
-    "User-Focused"
+  "The Unmatched Game Library",
+  "Speed and Performance",
+  "Unstoppable Access",
+  "The Responsive Design",
+  "Always Evolving",
+  "The Innovative Experience",
+  "User-Focused",
 ];
 var index = 0;
 
@@ -15,16 +15,29 @@ newDesc.style.opacity = 0;
 oldDesc.style.opacity = 0;
 
 function fadeDescriptors() {
-    newDesc.style.opacity = 0;
-    oldDesc.style.opacity = 1;
+  newDesc.style.opacity = 0;
+  oldDesc.style.opacity = 1;
 
-    setTimeout(() => {
-        index = (index + 1) % descriptors.length;
-        oldDesc.textContent = descriptors[index];
-        newDesc.textContent = descriptors[(index + 1) % descriptors.length]; 
-        newDesc.style.opacity = 1;
-        oldDesc.style.opacity = 0;
-    }, 1500);
+  setTimeout(() => {
+    index = (index + 1) % descriptors.length;
+    oldDesc.textContent = descriptors[index];
+    newDesc.textContent = descriptors[(index + 1) % descriptors.length];
+    newDesc.style.opacity = 1;
+    oldDesc.style.opacity = 0;
+  }, 1500);
+}
+
+function randomCapitalize(str) {
+  return str
+    .split("")
+    .map((char) =>
+      Math.random() < 0.5 ? char.toUpperCase() : char.toLowerCase()
+    )
+    .join("");
 }
 
 setInterval(fadeDescriptors, 3000);
+setInterval(function () {
+    oldDesc.textContent = randomCapitalize(oldDesc.textContent);
+    newDesc.textContent = randomCapitalize(newDesc.textContent);
+}, 300);
