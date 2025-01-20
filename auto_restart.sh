39f1d5192032e2d1d850634528b5203159a7f0c0
g-ames <132623120+g-ames@users.noreply.github.com>
2025-01-20 16:02:38 -0500

    Update auto_restart.sh

--- auto_restart.sh
+++ auto_restart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to start the server
 start_server() {
   echo "Starting server..."
   node ./server.js &
@@ -8,7 +7,6 @@ start_server() {
   echo "Server started with PID $SERVER_PID"
 }
 
-# Function to stop the server
 stop_server() {
   if [ -n "$SERVER_PID" ]; then
     echo "Stopping server with PID $SERVER_PID..."
@@ -18,22 +16,17 @@ stop_server() {
   fi
 }
 
-# Start the server initially
 start_server
 
-# Store the current remote commit hash
 LAST_COMMIT=$(git ls-remote origin -h refs/heads/$(git rev-parse --abbrev-ref HEAD) | awk '{ print $1 }')
 
 while true; do
-  sleep 5 # Check every 5 seconds
+  sleep 5
 
-  # Fetch the latest changes
   git fetch
 
-  # Get the latest commit hash from the remote
   NEW_COMMIT=$(git ls-remote origin -h refs/heads/$(git rev-parse --abbrev-ref HEAD) | awk '{ print $1 }')
 
-  # Compare hashes to detect changes
   if [ "$NEW_COMMIT" != "$LAST_COMMIT" ]; then
     echo "Detected changes in the Git repository."
     stop_server
@@ -42,7 +35,6 @@ while true; do
     git pull
     echo "Changes pulled."
 
-    # Update the stored commit hash
     LAST_COMMIT=$NEW_COMMIT
 
     start_server
