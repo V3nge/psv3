816c39d55dca42f4ee60f2903658d94ad30bd298
g-ames <132623120+g-ames@users.noreply.github.com>
2025-01-26 11:28:45 -0500

    yep

--- public/live-chat/blocked.js
+++ public/live-chat/blocked.js
@@ -1,4 +1,8 @@
 var allowedToChat = document.getElementById("allowed-to-chat");
 var blockedTime   = localStorage.getItem("blocked_time");
 
-console.log(JSON.parse(blockedTime));
+if(blockedTime == null) {
+    allowedToChat.innerText = allowedToChat.innerText.replace("...", "an undetermined amount of time.");
+} else {
+    console.log(JSON.parse(blockedTime));
+}
