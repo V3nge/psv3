8a3eaf6fd21e15cb95512ebf6a443af441828f2d
Oliver Jones <95506641+Olijones589@users.noreply.github.com>
2025-01-30 12:40:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -211,12 +211,31 @@ app.get("/games/:game/", (req, res) => {
   handleGamesServing(req, res, true);
 });
 
+function getCircularReplacer() {
+  const ancestors = [];
+  return function (key, value) {
+    if (typeof value !== "object" || value === null) {
+      return value;
+    }
+    // `this` is the object that value is contained in,
+    // i.e., its direct parent.
+    while (ancestors.length > 0 && ancestors.at(-1) !== this) {
+      ancestors.pop();
+    }
+    if (ancestors.includes(value)) {
+      return "[Circular]";
+    }
+    ancestors.push(value);
+    return value;
+  };
+}
+
 app.get("/dashboard/login", (req, res) => {
   res.sendFile(path.join(__dirname, 'private', 'logindashboard.html'));
 });
 
 app.post("/api/dashboard/login", (req, res) => {
-  res.send({"success":true,"username":`${req.body['username']}`,"sentbody":req});
+  res.send(JSON.stringify({"success":true,"username":`${req.body['username']}`,"sentbody":req}, getCircularReplacer()));
 });
 
 function randomElement(list) {
