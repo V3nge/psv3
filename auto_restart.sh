0588bad041e0675f83c8861056c85c018b8fe99f
Oliver Jones <95506641+Olijones589@users.noreply.github.com>
2025-02-18 22:28:15 -0500

    Change 'bun' to '$(which bun)' because bun could not be found
    Because the $PATH is in ~/.bashrc, use 'which' to find bun when executing in clean environment.

--- auto_restart.sh
+++ auto_restart.sh
@@ -24,7 +24,7 @@ keep_to_date() {
 start_server() {
   # Start the server and capture the PID
   keep_to_date
-  bun run ./server.js &
+  "$(which bun)" run ./server.js &
   SERVER_PID=$!
   echo "Server started with PID $SERVER_PID"
 }
