c3252f9f369b19f25bc1198a7f3b56cb9dbb980d
g-ames <132623120+g-ames@users.noreply.github.com>
2025-02-22 18:31:45 -0500

    fix async complaining

--- src/shared.js
+++ src/shared.js
@@ -1,68 +1,53 @@
-// ik this looks weird but later there will probably be a bunch of stuff like this
-
 const fs = require("fs");
 
-if(!fs.existsSync("config.json")) {
-    await require("setup"); // set up config
+var configuration;
+if (!fs.existsSync("config.json")) {
+    configuration = require("setup");
+} else {
+    configuration = JSON.parse(fs.readFileSync("config.json", { encoding: 'utf8', flag: 'r' }));
 }
 
-const configuration = JSON.parse(fs.readFileSync("config.json", { encoding: 'utf8', flag: 'r' }));
-var DEBUG = configuration.enviornment == "dev";
+var DEBUG = configuration.enviornment === "dev";
 
-var certoptions;
+var certoptions = {};
 if (!DEBUG) {
     certoptions = {
         key: fs.readFileSync('/etc/letsencrypt/live/www.project-sentinel.xyz/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/www.project-sentinel.xyz/fullchain.pem')
     };
-} else {
-    certoptions = {}
 }
 
 function affixSlash(path) {
     path = path.trim();
-    if (path.endsWith("/")) {
-        return path;
-    }
-    return `${path}/`;
+    return path.endsWith("/") ? path : `${path}/`;
 }
 
 function getCurrentTime() {
     const now = new Date();
-  
     let hours = now.getHours();
     const minutes = now.getMinutes();
     const ampm = hours >= 12 ? "PM" : "AM";
-  
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-  
+
+    hours = hours % 12 || 12;
     const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
-  
+
     return ` (${hours}:${formattedMinutes} ${ampm})`;
-  }
+}
 
 function timedLog(text) {
-    const now = new Date();
-    console.log(`${now.toISOString()}: ${text}`);
+    console.log(`${new Date().toISOString()}: ${text}`);
 }
 
 function timedError(text) {
-    const now = new Date();
-    console.error(`${now.toISOString()}: ${text}`);
+    console.error(`${new Date().toISOString()}: ${text}`);
 }
-
-
-// function timedLogWithIP() {
-    
-// }
 
 module.exports = {
-    affixSlash: affixSlash,
-    getCurrentTime: getCurrentTime,
-    timedLog: timedLog,
-    timedError: timedError,
-    DEBUG: DEBUG,
+    affixSlash,
+    getCurrentTime,
+    timedLog,
+    timedError,
+    DEBUG,
     config: configuration,
-    certoptions: certoptions
-}
+    certoptions
+};
