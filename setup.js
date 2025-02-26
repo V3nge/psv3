3b2e9061127848a7336f678d3a78709148251b05
G-ames <olijones589@gmail.com>
2025-02-25 19:21:17 -0500

    fix

--- setup.js
+++ setup.js
@@ -16,9 +16,9 @@ const askLimitedQuestion = (question, options) => {
     return new Promise(async (resolve) => {
         let answer = '';
         while (!options.includes(answer)) {
-            answer = await askQuestion(${question} (${options.join(', ')}): );
+            answer = await askQuestion(`${question} (${options.join(', ')}): `);
             if (!options.includes(answer)) {
-                console.log(Invalid input. Please choose one of the following: ${options.join(', ')});
+                console.log(`Invalid input. Please choose one of the following: ${options.join(', ')}`);
             }
         }
         resolve(answer);
@@ -29,17 +29,17 @@ const setup = async () => {
     config.environment = await askLimitedQuestion('Enter environment', ['dev', 'prod']);
     config.ports = await askQuestion('Enter ports to run the server on (comma separated, default=7764): ');
     config.proxy = await askLimitedQuestion('Configure the proxy', ['uv', 'native']);
-    config.httpmin = await askLimitedQuestion('Include http-min', ["y", "n"]) == "y";
-    if (config.ports == "") { config.ports = "7764"; }
+    config.httpmin = await askLimitedQuestion('Include http-min', ["y", "n"]) === "y";
+    if (config.ports === "") { config.ports = "7764"; }
 
     rl.close();
 
     fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
 
     console.log('\nconfig.json contains:');
-    console.log(\tEnvironment: ${config.environment});
-    console.log(\tPorts: ${config.ports});
-    console.log(\tProxy: ${config.proxy});
+    console.log(`\tEnvironment: ${config.environment}`);
+    console.log(`\tPorts: ${config.ports}`);
+    console.log(`\tProxy: ${config.proxy}`);
 
     return config;
 };
