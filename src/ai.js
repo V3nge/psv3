d7537c36fb00ddd7e1701aabd5e0f0250557c8f7
g-ames <95506641+Olijones589@users.noreply.github.com>
2025-04-18 20:01:02 -0400

    lets go

--- src/ai.js
+++ src/ai.js
@@ -1,21 +1,30 @@
 const fs = require("fs");
 const OpenAI = require('openai');
 
-var keys = fs.readFileSync('./secret.txt', 'utf8').split("\n");
+var keys;
+if (fs.existsSync('./secret.txt')) {
+  keys = fs.readFileSync('./secret.txt', 'utf8').split("\n");
+  
+  var openais = [];
 
-var openais = [];
-
-// console.log("Keys: ");
-keys.forEach(key => {
-  openais.push(new OpenAI({
-    apiKey: key.trim(),
-    baseURL: "https://api.aimlapi.com/v1"
-  }));
-//   console.log(`${key.trim()}`);
-})
-// console.log("");
+  // console.log("Keys: ");
+  keys.forEach(key => {
+    openais.push(new OpenAI({
+      apiKey: key.trim(),
+      baseURL: "https://api.aimlapi.com/v1"
+    }));
+  //   console.log(`${key.trim()}`);
+  })
+  // console.log("");
+} else {
+  console.info("[AI.JS]: NOTE: NO secret.txt, insert keys with each ending with a newline. (please do not include blank lines)");
+}
 
 async function createCompletion(messagesAi) {
+  if(keys == undefined) {
+    return "";
+  }
+
   const completion = await openais[Math.floor(Math.random() * openais.length)].chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: messagesAi,
